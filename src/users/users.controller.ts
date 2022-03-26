dbec013f8b0692711e6e6e1e6c4a63ac16e96262
David Yusta <david.yusta@guidesmiths.com>
2022-03-26 23:01:23 +0100

    "feat: use custom interceptor with user dto"

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -12,6 +12,8 @@ import {
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
+import { Serialize } from '../interceptors/serialize.interceptor';
+import { UserDto } from './dtos/user.dto';
 
 @Controller('auth')
 export class UsersController {
@@ -22,6 +24,8 @@ export class UsersController {
     this.usersService.create(body.email, body.password);
   }
 
+  // @UseInterceptors(new SerializeInterceptor(UserDto))
+  @Serialize(UserDto)
   @Get('/:id')
   async findUser(@Param('id') id: string) {
     const user = await this.usersService.findOne(parseInt(id));
