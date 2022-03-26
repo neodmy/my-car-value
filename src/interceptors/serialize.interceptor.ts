e03c9c5f21b3ee9be0de80ec91fc4cf58324bf7f
David Yusta <david.yusta@guidesmiths.com>
2022-03-26 23:00:46 +0100

    "feat: add custom serialize interceptor"

---
+++ src/interceptors/serialize.interceptor.ts
@@ -0,0 +1,30 @@
+import {
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+  UseInterceptors,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { plainToInstance } from 'class-transformer';
+
+export function Serialize(dto: any) {
+  return UseInterceptors(new SerializeInterceptor(dto));
+}
+
+export class SerializeInterceptor implements NestInterceptor {
+  constructor(private dto: any) {}
+
+  intercept(
+    context: ExecutionContext,
+    handler: CallHandler<any>,
+  ): Observable<any> {
+    return handler.handle().pipe(
+      map((data: any) => {
+        return plainToInstance(this.dto, data, {
+          excludeExtraneousValues: true, // exclude properties that don't have Exclude decorator
+        });
+      }),
+    );
+  }
+}
