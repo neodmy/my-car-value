d546fc93aa10a5b61e47c477c73b93628ce1614a
David Yusta <david.yusta@guidesmiths.com>
2022-03-29 20:59:02 +0200

    "feat: inject current user interceptor"

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -4,10 +4,13 @@ import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { AuthService } from './auth.service';
 import { User } from './user.entity';
+// The interceptor is injectable, so must be added to providers
+// The interceptor needs to run before the decorator picks up the user
+import { CurrentUserInterceptor } from './interceptors/current-user.interceptor';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])], // This creates a repository internally
   controllers: [UsersController],
-  providers: [UsersService, AuthService],
+  providers: [UsersService, AuthService, CurrentUserInterceptor],
 })
 export class UsersModule {}
