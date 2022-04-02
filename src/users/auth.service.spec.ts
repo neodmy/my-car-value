53ff224ccec891f1c6689ea9ae3e32b501765d31
David Yusta <david.yusta@guidesmiths.com>
2022-04-02 14:11:36 +0200

    "test: refactor adding describe and beforeEach"

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -3,25 +3,31 @@ import { AuthService } from './auth.service';
 import { UsersService } from './users.service';
 import { User } from './user.entity';
 
-it('can create an instance of auth service', async () => {
-  // Create UsersService mock with the functions AuthService needs from it
-  const fakeUsersService: Partial<UsersService> = {
-    find: () => Promise.resolve([]),
-    create: (email: string, password: string) =>
-      Promise.resolve({ id: 1, email, password } as User),
-  };
-  // Create DI Container
-  const module = await Test.createTestingModule({
-    providers: [
-      AuthService,
-      {
-        provide: UsersService,
-        useValue: fakeUsersService,
-      },
-    ],
-  }).compile();
+describe('AuthService', () => {
+  let service: AuthService;
 
-  const service = module.get(AuthService); // create an instance with its dependencies
+  beforeEach(async () => {
+    // Create UsersService mock with the functions AuthService needs from it
+    const fakeUsersService: Partial<UsersService> = {
+      find: () => Promise.resolve([]),
+      create: (email: string, password: string) =>
+        Promise.resolve({ id: 1, email, password } as User),
+    };
+    // Create DI Container
+    const module = await Test.createTestingModule({
+      providers: [
+        AuthService,
+        {
+          provide: UsersService,
+          useValue: fakeUsersService,
+        },
+      ],
+    }).compile();
 
-  expect(service).toBeDefined();
+    service = module.get(AuthService); // create an instance with its dependencies
+  });
+
+  it('can create an instance of auth service', async () => {
+    expect(service).toBeDefined();
+  });
 });
