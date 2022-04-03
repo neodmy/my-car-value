181a6923cf4fbf79e43fd978810a5410cd750ace
David Yusta <david.yusta@guidesmiths.com>
2022-04-03 10:14:30 +0200

    "refactor: use globally scoped middleware"

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module, ValidationPipe } from '@nestjs/common';
+import { MiddlewareConsumer, Module, ValidationPipe } from '@nestjs/common';
 import { APP_PIPE } from '@nestjs/core';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
@@ -7,6 +7,8 @@ import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
 import { User } from './users/user.entity';
 import { Report } from './reports/report.entity';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const cookieSession = require('cookie-session');
 
 @Module({
   imports: [
@@ -30,4 +32,15 @@ import { Report } from './reports/report.entity';
     },
   ],
 })
-export class AppModule {}
+export class AppModule {
+  // runs when the app starts listening for incoming requests
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(
+        cookieSession({
+          keys: ['asdfgh'], // @TODO: remove hardcoded key
+        }),
+      )
+      .forRoutes('*'); // For every request
+  }
+}
