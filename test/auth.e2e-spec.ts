03b62b375cb5032c1c406c101934b7d1b772ce20
David Yusta <david.yusta@guidesmiths.com>
2022-04-03 10:38:59 +0200

    "refactor: remove setupApp"

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -2,7 +2,6 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from '../src/app.module';
-import { setupApp } from '../src/setup-app';
 
 describe('Authentication System', () => {
   let app: INestApplication;
@@ -13,12 +12,11 @@ describe('Authentication System', () => {
     }).compile();
 
     app = moduleFixture.createNestApplication();
-    setupApp(app);
     await app.init();
   });
 
   it('handles a signup request', () => {
-    const email = 'email1@email.com';
+    const email = 'email2@email.com';
     return request(app.getHttpServer())
       .post('/auth/signup')
       .send({ email, password: 'password' })
